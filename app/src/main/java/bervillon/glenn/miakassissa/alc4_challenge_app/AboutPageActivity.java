1193e7a8be63c7b007af43191e0c93aeafcb69c1
miakassissa <bgmiakassiss@gmail.com>
2019-07-16 22:24:50 +0100

    Changes Made In AboutPageActivity.java About page "blank page" problem solved!

--- app/src/main/java/bervillon/glenn/miakassissa/alc4_challenge_app/AboutPageActivity.java
+++ app/src/main/java/bervillon/glenn/miakassissa/alc4_challenge_app/AboutPageActivity.java
@@ -2,8 +2,10 @@ package bervillon.glenn.miakassissa.alc4_challenge_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -17,7 +19,18 @@ public class AboutPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about_page);
 
         webView = (WebView) findViewById(R.id.about_page_webview);
-        webView.setWebViewClient(new WebViewClient());
+
+        /* This line under here was showing a blank page */
+        //webView.setWebViewClient(new WebViewClient());
+
+        /* Turning it to this, solved the blank page problem up here addressed */
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://andela.com/alc/");
     }
